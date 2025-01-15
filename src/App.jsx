b221a681054cb60757f565a2059419c3c868298b
YuriLopess <costalopesyuri@gmail.com>
2025-01-14 23:08:11 -0300

    feat: add code in header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { Outlet } from 'react-router-dom';
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer';
+import React from 'react'
+import "./app.css"
+import Header from "./components/header/Header"
 
-function App() {
+const App = () => {
   return (
     <>
       <Header/>
-      <Outlet />
-      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
